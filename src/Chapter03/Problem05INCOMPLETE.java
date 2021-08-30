13ca3b93048527367115c97288a229f71887dfa4
Colin Nordquist <colinnordquist@macbook-pro.lan>
2021-08-30 12:37:21 -0700

    chapter 3 in progress

--- src/Chapter03/Problem05INCOMPLETE.java
+++ src/Chapter03/Problem05INCOMPLETE.java
@@ -5,6 +5,6 @@ package Chapter03;
  * an additional temporary stack, but you may not copy the elements into any other data structure
  * (such as an array). The stack supports the following operations: push, pop, peek, and is Empty.
  */
-public class Problem05 {
+public class Problem05INCOMPLETE {
 
 }
